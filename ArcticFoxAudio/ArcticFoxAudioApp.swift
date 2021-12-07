559607e459811a57f7ce743ffabd20d7258928c0
Haydenfrea <hayden868686@gmail.com>
2021-12-06 19:08:31 -0500

    Initial Commit

---
+++ ArcticFoxAudio/ArcticFoxAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcticFoxAudioApp.swift
+//  ArcticFoxAudio
+//
+//  Created by hayden frea on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ArcticFoxAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArcticFoxAudio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArcticFoxAudio
+//
+//  Created by hayden frea on 12/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
