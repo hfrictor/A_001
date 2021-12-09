4977d327a3869165957282737b0c0410d2a43278
Haydenfrea <hayden868686@gmail.com>
2021-12-08 20:30:04 -0500

    Some changes

--- ArcticFoxAudio/ContentViews/SignUpView.swift
+++ ArcticFoxAudio/ContentViews/SignUpView.swift
@@ -7,14 +7,11 @@
 
 import SwiftUI
 
-struct SignInView: View {
+struct SignUpView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct SignInView_Previews: PreviewProvider {
-    static var previews: some View {
-        SignInView()
-    }
+
 }
